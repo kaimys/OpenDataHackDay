4cf8908d6764315f8e3631abe39a77f39b62791f
Devran Ünal <devran@me.com>
2014-06-15 15:47:20 +0200

    Push data from olcay, update markup

---
+++ htdocs/data/output.js
@@ -0,0 +1,641 @@
+﻿outputData = {
+  "totals": {
+    "count": 634,
+    "badboys": 122
+  },
+  "parties": [{
+    "name": "SPD",
+    "color": "#44FFCC",
+    "count": 110
+  }, {
+    "name": "BÜNDNIS 90/ DIE GRÜNEN",
+    "color": "#44FFCC",
+    "count": 53
+  }, {
+    "name": "CSU",
+    "color": "#44FFCC",
+    "count": 5
+  }, {
+    "name": "CDU",
+    "color": "#44FFCC",
+    "count": 32
+  }, {
+    "name": "DIE LINKE.",
+    "color": "#44FFCC",
+    "count": 19
+  }],
+  "badboys": [
+    {
+      "party": "SPD",
+      "name": "Petra Hinz",
+      "count": 12,
+      "picture": "ad7ea26d-f168-41e3-ab09-de62d3d7d53b.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Waltraud Wolff",
+      "count": 10,
+      "picture": "6da3eb7a-a598-4f28-9d0f-4ff880a2781d.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Marco Bülow",
+      "count": 9,
+      "picture": "cde35ebc-2d9e-4859-8cf0-2d15fd463d8f.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Klaus Barthel",
+      "count": 8,
+      "picture": "9916e0ba-649b-4894-b0e6-cce3cfe79791.jpg"
+    }, {
+      "party": "BÜNDNIS 90/ DIE GRÜNEN",
+      "name": "Monika Lazar",
+      "count": 7,
+      "picture": "962645ec-4650-467e-8fed-f231a022269b.jpg"
+    }, {
+      "party": "BÜNDNIS 90/ DIE GRÜNEN",
+      "name": "Hans-Christian Ströbele",
+      "count": 6,
+      "picture": "78624205-27af-4211-969b-d147d12cc640.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Cansel Kiziltepe",
+      "count": 6,
+      "picture": "adae65f8-0eed-4ca2-ae54-335fbdfd7261.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Hilde Mattheis",
+      "count": 5,
+      "picture": "d00f3cb5-e187-4aea-acc2-fbee1aec74c4.jpg"
+    }, {
+      "party": "BÜNDNIS 90/ DIE GRÜNEN",
+      "name": "Sylvia Kotting-Uhl",
+      "count": 4,
+      "picture": "84522938-105c-4fb3-9408-2b727b47fc31.jpg"
+    }, {
+      "party": "BÜNDNIS 90/ DIE GRÜNEN",
+      "name": "Peter Meiwald",
+      "count": 4,
+      "picture": "fa4a5a23-683f-4cf1-9622-fab7ad6fa84c.jpg"
+    }, {
+      "party": "BÜNDNIS 90/ DIE GRÜNEN",
+      "name": "Beate Müller-Gemmeke",
+      "count": 4,
+      "picture": "04eda6d0-0c3c-4d41-b453-6aefba1f5f40.jpg"
+    }, {
+      "party": "BÜNDNIS 90/ DIE GRÜNEN",
+      "name": "Corinna Rüffer",
+      "count": 4,
+      "picture": "f2bceb6f-2521-48ce-90b1-44457e7a5679.jpg"
+    }, {
+      "party": "BÜNDNIS 90/ DIE GRÜNEN",
+      "name": "Lisa Paus",
+      "count": 3,
+      "picture": "b8262af4-35ff-4c63-8871-5e0d707ed98b.jpg"
+    }, {
+      "party": "SPD",
+      "name": "René Röspel",
+      "count": 3,
+      "picture": "8f3db10d-090b-44ae-a677-7f800db6f2ab.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Swen Schulz",
+      "count": 3,
+      "picture": "87c7d085-83a1-469f-8fc7-0b4287a81d39.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Gabriele Hiller-Ohm",
+      "count": 3,
+      "picture": "a17ea476-63b9-49e1-8333-feddae061595.jpg"
+    }, {
+      "party": "CDU",
+      "name": "Martin Patzelt",
+      "count": 3,
+      "picture": "d55718a1-aa47-4dcf-8a46-2a8bc8fba1da.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Ewald Schurer",
+      "count": 3,
+      "picture": "4550574e-7ba0-4411-80a3-f52f8b955e17.jpg"
+    }, {
+      "party": "BÜNDNIS 90/ DIE GRÜNEN",
+      "name": "Julia Verlinden",
+      "count": 3,
+      "picture": "c9f91d85-9ee5-4345-bdb2-79aa98a231bd.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Frank Schwabe",
+      "count": 2,
+      "picture": "f3d0fec4-fe5b-4c44-9a53-0d182611d2aa.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Detlev Pilger",
+      "count": 2,
+      "picture": "70dac432-b8c4-463d-abc7-fd03f1f81a0d.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Stefan Rebmann",
+      "count": 2,
+      "picture": "bceb17c9-dada-471f-aeca-cb4c4cb8010b.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Susann Rüthrich",
+      "count": 2,
+      "picture": "9b0c6902-36dc-4101-8000-530751966ab9.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Stefan Zierke",
+      "count": 2,
+      "picture": "11b5b514-a8ff-4bf4-bfbd-6ddf7b11fac7.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Christian Petry",
+      "count": 2,
+      "picture": "17bcc1da-48c8-47aa-aabc-8834df234ef6.jpg"
+    }, {
+      "party": "CSU",
+      "name": "Josef Göppel",
+      "count": 2,
+      "picture": "ab22348c-5b60-4d1e-a0c3-b9574c020429.jpg"
+    }, {
+      "party": "CDU",
+      "name": "Johannes Selle",
+      "count": 2,
+      "picture": "1376d343-ba63-48e9-b2b9-abe66454caad.jpg"
+    }, {
+      "party": "CDU",
+      "name": "Reinhard Grindel",
+      "count": 2,
+      "picture": "0d6f8e65-1f9f-43c4-9c57-8bbe810742f9.jpg"
+    }, {
+      "party": "CDU",
+      "name": "Ulrich Petzold",
+      "count": 2,
+      "picture": "73cbc851-59c5-4288-805b-1e4a1c4ea35d.jpg"
+    }, {
+      "party": "CDU",
+      "name": "Dieter Stier",
+      "count": 2,
+      "picture": "e0a86aa6-802a-486f-854f-4f98de4eee55.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Ralf Kapschack",
+      "count": 2,
+      "picture": "cde8ea6b-5cb8-42c3-86e5-16f05e9113a2.jpg"
+    }, {
+      "party": "BÜNDNIS 90/ DIE GRÜNEN",
+      "name": "Harald Terpe",
+      "count": 2,
+      "picture": "890415cd-eb5c-4c38-970f-a870e09052df.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Rüdiger Veit",
+      "count": 2,
+      "picture": "94fc726c-fea1-453b-a620-5cae35dd8ed1.jpg"
+    }, {
+      "party": "BÜNDNIS 90/ DIE GRÜNEN",
+      "name": "Maria Klein-Schmeink",
+      "count": 2,
+      "picture": "a4fc0ea1-1f87-4773-864c-99153593a1e2.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Sonja Steffen",
+      "count": 2,
+      "picture": "a43661d6-6a8e-45a3-88a9-309900f8b745.jpg"
+    }, {
+      "party": "CSU",
+      "name": "Katrin Albsteiger",
+      "count": 1,
+      "picture": "3b2775f2-ecc8-4a87-8e98-c6859692acad.jpg"
+    }, {
+      "party": "CDU",
+      "name": "Mark Hauptmann",
+      "count": 1,
+      "picture": "c2223ef9-8230-415c-b828-def8397599be.jpg"
+    }, {
+      "party": "CDU",
+      "name": "Carsten Körber",
+      "count": 1,
+      "picture": "4d04758f-6040-4056-8b43-e3565723a365.jpg"
+    }, {
+      "party": "CDU",
+      "name": "Johannes Steiniger",
+      "count": 1,
+      "picture": "eafa3b4d-919c-4420-b3b1-2b274aff441b.jpg"
+    }, {
+      "party": "CDU",
+      "name": "Marian Wendt",
+      "count": 1,
+      "picture": "70166544-e106-4cdc-aa25-a117639dc3cc.jpg"
+    }, {
+      "party": "CDU",
+      "name": "Patricia Lips",
+      "count": 1,
+      "picture": "2847df61-ab0c-4e99-91b9-b24113ebb142.jpg"
+    }, {
+      "party": "CDU",
+      "name": "Eckhard Pols",
+      "count": 1,
+      "picture": "28e41048-2f49-45cb-bdde-1707bdacbbb2.jpg"
+    }, {
+      "party": "BÜNDNIS 90/ DIE GRÜNEN",
+      "name": "Jürgen Trittin",
+      "count": 1,
+      "picture": "ce1b06b0-2bae-4acf-a44a-14805cd1327e.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Edelgard Bulmahn",
+      "count": 1,
+      "picture": "99689982-dc22-4e1b-a432-ba76a4a669de.jpg"
+    }, {
+      "party": "CDU",
+      "name": "Cajus Caesar",
+      "count": 1,
+      "picture": "87e9c70e-aaae-42df-9d20-3ad57927ed89.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Johannes Kahrs",
+      "count": 1,
+      "picture": "773b1f11-17fe-45ce-bd2e-d8fefb0d67cf.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Christoph Strässer",
+      "count": 1,
+      "picture": "d2e2bc00-efe8-4503-9c52-00b02395cb0b.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Heinz-Joachim Barchmann",
+      "count": 1,
+      "picture": "6077b888-4ebb-46e6-9661-7f3106272118.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Dagmar Freitag",
+      "count": 1,
+      "picture": "4c7ac1ef-31b1-40f7-9960-49427bd411c8.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Dirk Heidenblut",
+      "count": 1,
+      "picture": "ad54f78b-d153-4cdd-aeb5-5a894a0f761c.jpg"
+    }, {
+      "party": "CDU",
+      "name": "Stefan Kaufmann",
+      "count": 1,
+      "picture": "c3b03901-6272-4233-adcb-e5e1daaa267c.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Mechthild Rawert",
+      "count": 1,
+      "picture": "59b1a04e-542c-4e3d-ba06-5e3ee80d0066.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Andreas Rimkus",
+      "count": 1,
+      "picture": "41571192-5409-4c0c-80f7-c3f59bdcfb60.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Gabi Weber",
+      "count": 1,
+      "picture": "c0aaa650-de62-41a9-9ace-6cf337be0202.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Andrea Wicklein",
+      "count": 1,
+      "picture": "4eef761b-82bb-4435-b058-03e78d74368e.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Helga Kühn-Mengel",
+      "count": 1,
+      "picture": "ed960802-1c62-4829-9a41-53b48c93ce6b.jpg"
+    }, {
+      "party": "DIE LINKE.",
+      "name": "Roland Claus",
+      "count": 1,
+      "picture": "c5b3c319-d038-4c76-b542-9e139ddb558c.jpg"
+    }, {
+      "party": "DIE LINKE.",
+      "name": "Katrin Kunert",
+      "count": 1,
+      "picture": "3af70d00-a06c-442a-9fa8-3e322a439e7b.jpg"
+    }, {
+      "party": "DIE LINKE.",
+      "name": "Michael Leutert",
+      "count": 1,
+      "picture": "ce4218f8-bcbd-4436-9188-dbbf32a1bf8b.jpg"
+    }, {
+      "party": "DIE LINKE.",
+      "name": "Stefan Liebich",
+      "count": 1,
+      "picture": "da110872-c454-48b0-b09c-4ac649212487.jpg"
+    }, {
+      "party": "DIE LINKE.",
+      "name": "Diana Golze",
+      "count": 1,
+      "picture": "5fb8adcf-6aea-4137-a8f4-ce8b1c0a0564.jpg"
+    }, {
+      "party": "DIE LINKE.",
+      "name": "Susanna Karawanskij",
+      "count": 1,
+      "picture": "93c6ef4e-21f8-4ab9-960b-ba4b18367acb.jpg"
+    }, {
+      "party": "DIE LINKE.",
+      "name": "Jan Korte",
+      "count": 1,
+      "picture": "c667afb6-a2d3-49ed-9b2a-1f7440cb19da.jpg"
+    }, {
+      "party": "DIE LINKE.",
+      "name": "Caren Lay",
+      "count": 1,
+      "picture": "2b5c57f7-25ab-4345-b1d4-e2f4cd3fe34e.jpg"
+    }, {
+      "party": "DIE LINKE.",
+      "name": "Ralph Lenkert",
+      "count": 1,
+      "picture": "9464c1f5-3c00-444f-88a1-f042ea4c9c88.jpg"
+    }, {
+      "party": "DIE LINKE.",
+      "name": "Thomas Lutze",
+      "count": 1,
+      "picture": "fcc51aa0-f054-497b-8a0e-eebb6b78e244.jpg"
+    }, {
+      "party": "DIE LINKE.",
+      "name": "Thomas Nord",
+      "count": 1,
+      "picture": "5ecbc283-6b37-4fbb-bd4d-0f347f811667.jpg"
+    }, {
+      "party": "DIE LINKE.",
+      "name": "Petra Pau",
+      "count": 1,
+      "picture": "539d762c-7f50-41be-b8d3-ffa2aa79d52a.jpg"
+    }, {
+      "party": "DIE LINKE.",
+      "name": "Harald Petzold",
+      "count": 1,
+      "picture": "775b7b74-15bd-4607-98b9-caf3a33d26ec.jpg"
+    }, {
+      "party": "DIE LINKE.",
+      "name": "Kersten Steinke",
+      "count": 1,
+      "picture": "67a020e4-cc3b-499c-b77b-efd000fa8f84.jpg"
+    }, {
+      "party": "DIE LINKE.",
+      "name": "Frank Tempel",
+      "count": 1,
+      "picture": "326bdb45-c2c8-4b2d-9552-6145b2f17e65.jpg"
+    }, {
+      "party": "DIE LINKE.",
+      "name": "Birgit Wöllert",
+      "count": 1,
+      "picture": "211ab867-ade9-4769-b221-e1dee782e325.jpg"
+    }, {
+      "party": "CDU",
+      "name": "Steffen Bilger",
+      "count": 1,
+      "picture": "36e162be-1f1b-431f-ab9a-d0574ac43064.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Gabriele Groneberg",
+      "count": 1,
+      "picture": "9cd6a454-d92c-4972-a8ed-533ba9a9070f.jpg"
+    }, {
+      "party": "CSU",
+      "name": "Alexander Hoffmann",
+      "count": 1,
+      "picture": "9d4c81d1-f2bb-4780-bcfe-6f58afa52d08.jpg"
+    }, {
+      "party": "CDU",
+      "name": "Armin Schuster",
+      "count": 1,
+      "picture": "c3a26c1b-6911-4783-bb0d-1e0c7fd22e8d.jpg"
+    }, {
+      "party": "CDU",
+      "name": "Joachim Pfeiffer",
+      "count": 1,
+      "picture": "c81f6649-2674-4ad2-87ef-d4d343dd30ed.jpg"
+    }, {
+      "party": "CDU",
+      "name": "Norbert Röttgen",
+      "count": 1,
+      "picture": "32f35b1b-9160-4275-9042-346103e05fd0.jpg"
+    }, {
+      "party": "CDU",
+      "name": "Karl-Georg Wellmann",
+      "count": 1,
+      "picture": "521a76df-8f4a-4f72-b3c1-041175425203.jpg"
+    }, {
+      "party": "CDU",
+      "name": "Mathias Middelberg",
+      "count": 1,
+      "picture": "109fa4a4-854f-4339-ab4e-bdaab90d402e.jpg"
+    }, {
+      "party": "CSU",
+      "name": "Peter Ramsauer",
+      "count": 1,
+      "picture": "08554e2e-98c1-47a9-9f31-eddaadd8fd8b.jpg"
+    }, {
+      "party": "CDU",
+      "name": "Ingo Wellenreuther",
+      "count": 1,
+      "picture": "349355d2-7297-4597-9e3e-2a2d8f71aa8c.jpg"
+    }, {
+      "party": "CDU",
+      "name": "Kordula Kovac",
+      "count": 1,
+      "picture": "eb239553-d43a-4c30-b8f9-98fc984b0e78.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Andreas Schwarz",
+      "count": 1,
+      "picture": "0755acc2-cc78-48ee-9351-3c6dbbc7e4c2.jpg"
+    }, {
+      "party": "DIE LINKE.",
+      "name": "Klaus Ernst",
+      "count": 1,
+      "picture": "ec26b137-9778-4752-8565-92808705bbc3.jpg"
+    }, {
+      "party": "DIE LINKE.",
+      "name": "Richard Pitterle",
+      "count": 1,
+      "picture": "c5bf64b4-4f09-40b8-b1bb-cd0feac8be45.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Achim Post",
+      "count": 1,
+      "picture": "e6a76034-7212-4ce1-b8d1-e15918482247.jpg"
+    }, {
+      "party": "DIE LINKE.",
+      "name": "Jörn Wunderlich",
+      "count": 1,
+      "picture": "785e4b4c-9ba8-4850-a519-e33a22929065.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Bärbel Bas",
+      "count": 1,
+      "picture": "1a8bb92c-b1ce-4eb7-a8b4-334db7044e5e.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Wolfgang Gunkel",
+      "count": 1,
+      "picture": "f9468f3b-ed53-4dc0-ae84-07f56b9b9c38.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Daniela Kolbe",
+      "count": 1,
+      "picture": "8dd6f066-ec43-412b-808a-22a3c8cff715.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Wilhelm Priesmeier",
+      "count": 1,
+      "picture": "82e5daf8-49f5-48ee-a9bc-362b819ac70a.jpg"
+    }, {
+      "party": "CDU",
+      "name": "Norbert Schindler",
+      "count": 1,
+      "picture": "d252f21e-d708-4e2f-bda4-77b841474541.jpg"
+    }, {
+      "party": "BÜNDNIS 90/ DIE GRÜNEN",
+      "name": "Wolfgang Strengmann-Kuhn",
+      "count": 1,
+      "picture": "e372dee0-f2ee-4d9b-901b-cabbd423a536.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Kerstin Tack",
+      "count": 1,
+      "picture": "3583b7cf-35c2-4bd2-8fab-ec4561cd28d1.jpg"
+    }, {
+      "party": "BÜNDNIS 90/ DIE GRÜNEN",
+      "name": "Agnieszka Brugger",
+      "count": 1,
+      "picture": "6fc905ac-9ebf-49b5-bf92-3b0c2f2bc683.jpg"
+    }, {
+      "party": "BÜNDNIS 90/ DIE GRÜNEN",
+      "name": "Katja Dörner",
+      "count": 1,
+      "picture": "e4e4516d-22a0-4ac0-b3e3-c3eb33a43c3c.jpg"
+    }, {
+      "party": "BÜNDNIS 90/ DIE GRÜNEN",
+      "name": "Katharina Dröge",
+      "count": 1,
+      "picture": "67478631-1f09-4762-8de8-b2c2ad41e6f2.jpg"
+    }, {
+      "party": "BÜNDNIS 90/ DIE GRÜNEN",
+      "name": "Harald Ebner",
+      "count": 1,
+      "picture": "bb3f55be-637d-4336-9d89-2b95ca79ee12.jpg"
+    }, {
+      "party": "BÜNDNIS 90/ DIE GRÜNEN",
+      "name": "Matthias Gastel",
+      "count": 1,
+      "picture": "223c719c-4233-40e6-9409-682ab523c73f.jpg"
+    }, {
+      "party": "BÜNDNIS 90/ DIE GRÜNEN",
+      "name": "Bärbel Höhn",
+      "count": 1,
+      "picture": "81d79c87-558e-41b3-9140-77839bc239f1.jpg"
+    }, {
+      "party": "BÜNDNIS 90/ DIE GRÜNEN",
+      "name": "Uwe Kekeritz",
+      "count": 1,
+      "picture": "b8106a13-3a3f-487e-bafb-734a419f4d58.jpg"
+    }, {
+      "party": "BÜNDNIS 90/ DIE GRÜNEN",
+      "name": "Katja Keul",
+      "count": 1,
+      "picture": "5ab6d48b-cc7f-4f25-8a04-9ff6ca2d7960.jpg"
+    }, {
+      "party": "BÜNDNIS 90/ DIE GRÜNEN",
+      "name": "Sven-Christian Kindler",
+      "count": 1,
+      "picture": "51930075-e52a-47fe-9287-c5fbc3781b71.jpg"
+    }, {
+      "party": "BÜNDNIS 90/ DIE GRÜNEN",
+      "name": "Irene Mihalic",
+      "count": 1,
+      "picture": "6484c77c-8098-4000-9f2a-8e33f655e951.jpg"
+    }, {
+      "party": "BÜNDNIS 90/ DIE GRÜNEN",
+      "name": "Özcan Mutlu",
+      "count": 1,
+      "picture": "721cf3c5-f70c-406b-ba1c-97fa9b7c6f48.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Sönke Rix",
+      "count": 1,
+      "picture": "9be22d86-8639-4f0c-ac7c-ab4061153181.jpg"
+    }, {
+      "party": "BÜNDNIS 90/ DIE GRÜNEN",
+      "name": "Ulle Schauws",
+      "count": 1,
+      "picture": "a9530338-b3e7-4466-a74e-a6c1c385a829.jpg"
+    }, {
+      "party": "CDU",
+      "name": "Hubert Hüppe",
+      "count": 1,
+      "picture": "7578f79e-316b-4ada-bcbd-c4d72a2fb52b.jpg"
+    }, {
+      "party": "CDU",
+      "name": "Matern von Marschall",
+      "count": 1,
+      "picture": "1c7c1828-35ad-425d-a287-95372b7a2a44.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Elvira Drobinski-Weiß",
+      "count": 1,
+      "picture": "70c061a7-38e2-4e18-b656-90a23874c77b.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Uli Grötsch",
+      "count": 1,
+      "picture": "40b252be-991c-4c0a-867c-708d12defd26.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Bettina Hagedorn",
+      "count": 1,
+      "picture": "5a80e7ad-ced5-44d4-8d00-328ad8ddc18f.jpg"
+    }, {
+      "party": "CDU",
+      "name": "Frank Heinrich",
+      "count": 1,
+      "picture": "03a6c443-39f3-4cc4-8e47-bcae81104712.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Gustav Herzog",
+      "count": 1,
+      "picture": "7a666065-4350-432c-9f40-21097321dae3.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Thomas Hitschler",
+      "count": 1,
+      "picture": "cba6c788-60dd-4989-972f-62a586ae0dbd.jpg"
+    }, {
+      "party": "CDU",
+      "name": "Karl-Heinz (Charles M.) Huber",
+      "count": 1,
+      "picture": "1459ece8-414a-42e8-b414-0431612d72ee.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Steffen-Claudio Lemme",
+      "count": 1,
+      "picture": "a0765bbb-c7ee-4538-aa2c-5f69e6f854ff.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Klaus Mindrup",
+      "count": 1,
+      "picture": "818ac3b0-6a92-41c3-895a-216c5307663a.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Ursula Schulte",
+      "count": 1,
+      "picture": "08910732-bb47-46e4-9c4f-15d8b61a6dd0.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Gerold Reichenbach",
+      "count": 1,
+      "picture": "139cff2e-58a1-480f-a539-2959b80119c9.jpg"
+    }, {
+      "party": "SPD",
+      "name": "Willi Brase",
+      "count": 1,
+      "picture": "fddb34b5-8493-4d21-8a96-8802358096b5.jpg"
+    }
+  ],
+  "versus": []
+}
